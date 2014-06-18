e5588079f7b9da0ae291c87cf0132b80333edf5b
Warut Surapat <s.warut@gmail.com>
2014-06-18 16:21:43 +0700

    Update scheme.

---
+++ Prebot/Prebot.h
@@ -0,0 +1,13 @@
+//
+//  Prebot.h
+//  Prebot
+//
+//  Created by Warut Surapat on 6/18/14.
+//  Copyright (c) 2014 Taskworld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Prebot : NSObject
+
+@end
---
+++ Prebot/Prebot.m
@@ -0,0 +1,13 @@
+//
+//  Prebot.m
+//  Prebot
+//
+//  Created by Warut Surapat on 6/18/14.
+//  Copyright (c) 2014 Taskworld. All rights reserved.
+//
+
+#import "Prebot.h"
+
+@implementation Prebot
+
+@end
